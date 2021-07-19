1e527c4152f8106e770ee18d54278b310bc6df0b
bigeye2599 <bigeye2599@gmail.com>
2021-07-19 22:31:54 +0900

    메시지 수정, 삭제 기능 구현

--- client/components/MsgItem.js
+++ client/components/MsgItem.js
@@ -1,4 +1,15 @@
-const MsgItem = ({ userId, timeStamp, text }) => (
+import MsgInput from "./MsgInput";
+
+const MsgItem = ({
+  id,
+  userId,
+  timeStamp,
+  text,
+  onUpdate,
+  onDelete,
+  isEditing,
+  startEdit,
+}) => (
   <li className="messages__item">
     <h3>
       {userId}{" "}
@@ -14,7 +25,18 @@ const MsgItem = ({ userId, timeStamp, text }) => (
       </sub>
     </h3>
 
-    {text}
+    {isEditing ? (
+      <>
+        <MsgInput mutate={onUpdate} text={text} id={id} />
+      </>
+    ) : (
+      text
+    )}
+
+    <div className="messages__buttons">
+      <button onClick={startEdit}>수정</button>
+      <button onClick={onDelete}>삭제</button>
+    </div>
   </li>
 );
 
